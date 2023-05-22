29223f487f37da55eba97bf413b346ec777ecb3e
ngocon <133983535+ngocon@users.noreply.github.com>
2023-05-22 11:28:02 +0700

    Add files via upload
    bai_tap_2

--- Untitled.sql
+++ Untitled.sql
@@ -5,10 +5,12 @@ create table Trainee(
    `Full_Name` varchar(60),
    Brith_Date int,
    Gender  enum('male','female','unknown'),
-   ET_IQ  smallint unsigned
-   check (ET_IQ<21), 
-  
-   ET_English int,
-   Training_Class int,     
+   ET_IQ  int
+      check(0<=ET_IQ=>20),
+   ET_Gmanth　int
+      check(0<=ET_Gmanth=>20),
+   ET_English int 
+      check(0<=ET_English=>50),
+   Training_Class varchar(10),     
    Evanluation_Notes     varchar(250)
 );
