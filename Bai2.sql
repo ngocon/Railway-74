13508633d345a59ec001b671db54a1c0b007999f
ngocon <133983535+ngocon@users.noreply.github.com>
2023-05-22 21:13:38 +0700

    Add files via upload
    Extra_Assignment_2

---
+++ Bai2.sql
@@ -0,0 +1,16 @@
+create database Assignment_2;
+use Assignment_2;
+create table Trainee(
+   Trainee_ID  int auto_increment primary key,
+   Full_Name varchar(60),
+   Brith_Date date not null unique key,
+   Gender  enum('male','female','unknown'),
+   ET_IQ  int not null,
+      check (0<=ET_IQ<=20),
+   ET_Gmanth　int 
+       check (0<=ET_Gmanth<=20),
+   ET_English int 
+      check (0<=ET_English<=50),
+   Training_Class varchar(10),     
+   Evanluation_Notes     varchar(255)
+);
